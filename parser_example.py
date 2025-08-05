36ffbbf1bd12a5807a9bb69387372c65338297d9
Karla Rodriguez <karla.pipistrello@gmail.com>
2025-08-05 09:52:35 +0200

    example using readline

--- parser_example.py
+++ parser_example.py
@@ -41,9 +41,9 @@ class ASTNode:
 def parse_expression(tokens: list[Token]):
     def parse_term():
         node = parse_factor()
-        while tokens and tokens[0].type == "OPERATOR" and tokens[0].value == "*":
+        while tokens and tokens[0].type == "OPERATOR" and tokens[0].value == "*" :
             tokens.pop(0)
-            node = ASTNode("MULTIPLY", left=node, right=parse_factor())
+            node = ASTNode("MULTIPLY", '*', left=node, right=parse_factor())
         return node
 
     def parse_factor():
@@ -56,9 +56,61 @@ def parse_expression(tokens: list[Token]):
     node = parse_term()
     while tokens and tokens[0].type == "OPERATOR" and tokens[0].value == "+":
         tokens.pop(0)
-        node = ASTNode("ADD", left=node, right=parse_term())
+        node = ASTNode("ADD", '+', left=node, right=parse_term())
     return node
 
+def prefix_notation(node: Optional[ASTNode], first:bool, prefix: list[str])->list[str]:
+	if (node == None):
+		return prefix
+	if (first == True):
+		current = str(node.value)
+		prefix.append(current)
+	if (node.left == None):
+		return prefix
+	else:
+		current = str(node.left.value)
+		prefix.append(current)
+		if (node.right != None):
+			current = str(node.right.value)
+			prefix.append(current)
+		if (node.left.left != None):
+			prefix_notation(node.left, False, prefix)
+		if (node.right != None and node.right.left != None):
+			prefix_notation(node.right, False, prefix)
+		# else:
+		return prefix
+		# 	print(f"{level*blank} {tee} {node.left.value}")
+			# if (node.left.left != None):
+			# 	print_tree(node.left, False, level)
+			# print(f"{level*blank} {elbow} {node.right.value}")
+			# if (node.right.left != None):
+			# 	prefix_notation(node.right, False, prefix)
+
+def print_tree(node: Optional[ASTNode], first: bool , level: int):
+	elbow = "└──"
+#	pipe = "│  "
+	tee = "├──"
+	blank = "   "
+	if (node == None):
+		return
+	if (first == True):
+		print(f"{elbow} {node.value}")
+	if (node.left == None):
+		return
+	else:
+		level += 1
+		if (node.right == None):
+			print(f"{level*blank} {elbow} {node.left.value}")
+			if (node.left.left != None):
+				print_tree(node.left, False, level)
+		else:
+			print(f"{level*blank} {tee} {node.left.value}")
+			if (node.left.left != None):
+				print_tree(node.left, False, level)
+			print(f"{level*blank} {elbow} {node.right.value}")
+			if (node.right.left != None):
+				print_tree(node.right, False, level)
+		
 
 def evaluate_ast(node: Optional[ASTNode]) -> Any:
     if node == None:
@@ -66,15 +118,20 @@ def evaluate_ast(node: Optional[ASTNode]) -> Any:
     if node.type == "NUMBER":
         return node.value
     elif node.type == "ADD":
+        print(f"node = {node.value} left = {node.left.value} right = {node.right.value}")
         return evaluate_ast(node.left) + evaluate_ast(node.right)
     elif node.type == "MULTIPLY":
+        print(f"node = {node.value} left = {node.left.value} right = {node.right.value}")
         return evaluate_ast(node.left) * evaluate_ast(node.right)
 
 
 if __name__ == "__main__":
     # Example usage
-    expression = "3 + 4 * 2"
+    prefix : list[str] = []
+    expression = "7 + 3 + 8 * 2"
     tokens = tokenize(expression)
     ast = parse_expression(tokens)
+    print_tree(ast, True, 0)
+    prefix = prefix_notation(ast, True, prefix)
     result = evaluate_ast(ast)
-    print(f"The result of {expression} is {result}")
+    print(f"The result of {expression} {prefix} is {result}")
--- practice_readline.c
+++ practice_readline.c
@@ -1,11 +1,65 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <strings.h>
+#include <wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <limits.h>
 
 int	main(void)
 {
-	char	*line;
+	char		*line;
+	int 		i;
+	pid_t		pid;
+	pid_t		pidls;
+	int			statusls;
+	int			status;
+	extern char	**environ;
+	char	*args[] = {"clear", NULL};
+	char	*argsls[] = {"ls", NULL};
 
-	line = readline("Prueba: ");
-	
+	pid = fork();
+	if (pid == 0)
+	{
+		execve("/usr/bin/clear",args,environ);
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+	}
+	while(i == 0)
+	{
+		line = readline("Prueba: ");
+		if ( strcmp(line, "C") == 0)
+		{
+			i = 1;
+			printf("You entered: \"%s\"\n", line);
+			free(line);
+		}
+		else if (strcmp(line,"ls") == 0)
+		{
+			pidls = fork();
+			if (pidls == 0)
+			{
+				execve("/usr/bin/ls",argsls,environ);
+			}
+			else
+			{
+				waitpid(pidls, &statusls, 0);
+			}
+		}
+		else if (strcmp(line,"cd") == 0)
+		{
+			chdir("/home/karla/Documents/42_Cursus");
+		}
+		else
+			printf("You entered: \"%s\"\n", line);
+
+
+
+	}
+
+    return 0;
 }
